1f0b5247a4d7a8580ab1f76efd628c4e491ee970
Evan Rinehart <evanrinehart@gmail.com>
2022-12-01 19:28:02 -0600

    cleaned up day 1 code

--- 1/Main.hs
+++ 1/Main.hs
@@ -1,3 +1,23 @@
 module Main where
 
-main = pure ()
+import Data.List
+
+main = do
+  theData <- loadData "input"
+  let highScores = sortElves theData
+  let topThree = take 3 highScores
+  print (head topThree)
+  print (sum topThree)
+
+sortElves :: [[Int]] -> [Int]
+sortElves = reverse . sort . map sum
+
+loadData :: FilePath -> IO [[Int]]
+loadData = fmap f . readFile where
+  f = map (map read) . splitOn (=="") . lines 
+
+splitOn :: (a -> Bool) -> [a] -> [[a]]
+splitOn _ [] = []
+splitOn f l@(x:xs)
+  | f x = splitOn f xs
+  | otherwise = let (h,t) = break f l in h:(splitOn f t)
