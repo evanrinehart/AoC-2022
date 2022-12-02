15e0060a4db30d7a9243aa7335331c23ebdde1e5
Evan Rinehart <evanrinehart@gmail.com>
2022-12-02 07:44:44 -0600

    clean up day 2

--- 2/Main.hs
+++ 2/Main.hs
@@ -1,60 +1,60 @@
 module Main where
 
+data RPS     = Rock | Paper | Scissor deriving (Eq,Ord,Read,Show)
+data Outcome = Win | Draw | Lose deriving (Eq,Ord,Read,Show)
+
 main = do
-  theData <- loadData "input"
-  print (sum (map (score . v1) theData))
-  let hmm = map (ff . v2) theData
-  print (sum (map score hmm))
+  c1raw <- loadColumn1 "input"
+  c2raw <- loadColumn2 "input"
 
-ff (x,c) = let y = decrypt (x,c) in (x,y)
+  let c1 = map unABC c1raw
 
-v1 (x,'X') = (x,Rock)
-v1 (x,'Y') = (x,Paper)
-v1 (x,'Z') = (x,Scissor)
+  let c2 = map unXYZ c2raw
+  print (sum (zipWith score c1 c2))
 
-v2 (x,'X') = (x,Lose)
-v2 (x,'Y') = (x,Tie)
-v2 (x,'Z') = (x,Win)
+  let c2' = map unXYZ' c2raw
+  let c3  = zipWith decrypt c1 c2'
+  print (sum (zipWith score c1 c3))
 
-data RPS = Rock | Paper | Scissor deriving (Eq,Ord,Read,Show)
-data Outcome = Win | Tie | Lose deriving (Eq,Ord,Read,Show)
+unABC 'A' = Rock
+unABC 'B' = Paper
+unABC 'C' = Scissor
 
-pl (c1:' ':c2:_) = (f c1, c2) where
-  f 'A' = Rock
-  f 'B' = Paper
-  f 'C' = Scissor
+unXYZ 'X' = Rock
+unXYZ 'Y' = Paper
+unXYZ 'Z' = Scissor
 
-score :: (RPS,RPS) -> Int
-score (a,b) = rps a b + g b where
+unXYZ' 'X' = Lose
+unXYZ' 'Y' = Draw
+unXYZ' 'Z' = Win
+
+score :: RPS -> RPS -> Int
+score a b = rps a b + g b where
   g Rock    = 1
   g Paper   = 2
   g Scissor = 3
 
-xyz :: RPS -> Char
-xyz Rock = 'X'
-xyz Paper = 'Y'
-xyz Scissor = 'Z'
-
--- in question 2, which move to make to get the outcome
-decrypt :: (RPS,Outcome) -> RPS
-decrypt (a,b) = f a b where
-  f Rock Win = Paper
-  f Paper Win = Scissor
-  f Scissor Win = Rock
-  f x Tie = x
-  f Rock Lose = Scissor
-  f Paper Lose = Rock
-  f Scissor Lose = Paper
-
 rps = f where
-  f Rock Paper = 6
-  f Scissor Rock = 6
-  f Paper Scissor = 6
-  f Paper Rock = 0
-  f Rock Scissor = 0
-  f Scissor Paper = 0
-  f _ _ = 3
+  f Rock    Paper   = 6
+  f Scissor Rock    = 6
+  f Paper   Scissor = 6
+  f Paper   Rock    = 0
+  f Rock    Scissor = 0
+  f Scissor Paper   = 0
+  f _       _       = 3
 
-loadData :: FilePath -> IO [(RPS,Char)]
-loadData = fmap f . readFile where
-  f = map pl . lines
+decrypt :: RPS -> Outcome -> RPS
+decrypt x       Draw  = x
+decrypt Rock    Win  = Paper
+decrypt Paper   Win  = Scissor
+decrypt Scissor Win  = Rock
+decrypt Rock    Lose = Scissor
+decrypt Paper   Lose = Rock
+decrypt Scissor Lose = Paper
+
+loadColumn1 :: FilePath -> IO [Char]
+loadColumn1 = fmap (map head . lines) . readFile
+
+loadColumn2 :: FilePath -> IO [Char]
+loadColumn2 = fmap (map f . lines) . readFile where
+  f (_:_:c:_) = c
