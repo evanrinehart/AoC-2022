86ae83675e74384f85a2625d66771985f93e50fc
Evan Rinehart <evanrinehart@gmail.com>
2022-12-12 15:02:02 -0600

    working now, faster

--- 12/AStar.hs
+++ 12/AStar.hs
@@ -1,6 +1,7 @@
+--{-# LANGUAGE ScopedTypeVariables #-}
 module AStar where
 
-import Data.IntMap (IntMap); import qualified Data.IntMap as IM
+import Data.IntMap (IntMap, (!)); import qualified Data.IntMap as IM
 import Data.Maybe
 import Data.List
 import Data.Ord
@@ -21,11 +22,10 @@ findPathFromTo ps start end = go ps (makeGuts ps start) end
 data Guts p = Guts
   { fScores  :: IntMap Int
   , gScores  :: IntMap Int
-  , openSet  :: OpenSet p
-  , cameFrom :: IntMap p }
+  , openSet  :: [(Int,p)]
+  , cameFrom :: IntMap Int
+  , decoder  :: IntMap p }
       deriving Show
-
-type OpenSet a = [(Int,a)]
 
 makeGuts :: Ord p => PathSpace p -> p -> Guts p
 makeGuts PathSpace{heuristic=h,encodePoint=en} start =
@@ -34,46 +34,51 @@ makeGuts PathSpace{heuristic=h,encodePoint=en} start =
     { fScores   = IM.singleton s 0
     , gScores   = IM.singleton s (h start)
     , openSet   = [(0,start)]
-    , cameFrom  = IM.empty }
+    , cameFrom  = IM.empty
+    , decoder   = IM.singleton s start }
 
 -- main loop
 go :: Ord p => PathSpace p -> Guts p -> p -> [p]
 go ps guts dest =
-  let PathSpace{neighborhoodOf=nh} = ps   in
-  let Guts{openSet=os}             = guts in
+  let PathSpace{neighborhoodOf=nh,encodePoint=en} = ps   in
+  let Guts{openSet=os,decoder=dc}  = guts in
   let (current,os')                = getCurrent guts in
   if current == dest
-    then reconstructPath guts current
+    then map (dc !) (reconstructPath guts (en current))
     else go2 ps dest current (nh current) guts{openSet=os'}
 
 -- try all neighbors of current
 go2 :: Ord p => PathSpace p -> p -> p -> [p] -> Guts p -> [p]
-go2 ps dest current []     guts = go ps guts dest 
-go2 ps dest current (x:xs) guts = 
-  let PathSpace{linkWeight=lw,heuristic=h} = ps in
-  let Guts{fScores=fs,gScores=gs,openSet=os,cameFrom=cf} = guts in
-  let tentativeG = getG guts current + lw current x in
-  case tentativeG < getG guts x of
-    False -> go2 ps dest current xs guts
-    True  -> go2 ps dest current xs
-      Guts { fScores  = M.insert x fscore fs
-           , gScores  = M.insert x tentativeG gs
-           , openSet  = pinsert (fscore,x) os
-           , cameFrom = M.insert x current cf } where fscore = tentativeG + h x
+go2 space dest current []     guts = go space guts dest 
+go2 space dest current (p:ps) guts = 
+  let PathSpace{linkWeight=lw,heuristic=h,encodePoint=en} = space
+      Guts{fScores=fs,gScores=gs,openSet=os,cameFrom=cf,decoder=dc} = guts
+      x = en p
+      currentx = en current
+      tentativeG = getG guts currentx + lw current p
+      fscore = tentativeG + h p
+  in case tentativeG < getG guts x of
+    False -> go2 space dest current ps guts
+    True  -> go2 space dest current ps
+      Guts { fScores  = IM.insert x fscore fs
+           , gScores  = IM.insert x tentativeG gs
+           , openSet  = pinsert (fscore,p) os
+           , cameFrom = IM.insert x currentx cf
+           , decoder  = IM.insert x p dc }
 
 -- take point with lowest? F score from the openSet
-getCurrent :: Ord p => Guts p -> (p, OpenSet p)
+getCurrent :: Guts p -> (p, [(Int,p)])
 getCurrent Guts{openSet=(_,p):q} = (p,q)
 
 -- G score of point
-getG :: Ord p => Guts p -> p -> Int
-getG guts p = fromMaybe maxBound (M.lookup p (gScores guts))
+getG :: Guts p -> Int -> Int
+getG guts x = fromMaybe maxBound (IM.lookup x (gScores guts))
 
-reconstructPath :: Ord p => (p -> Int) -> Guts p -> p -> [p]
-reconstructPath en Guts{cameFrom=cf} end = go [] end where
-  go ps p = case IM.lookup (en p) cf of
-    Nothing -> p:ps
-    Just p' -> go (p:ps) p'
+reconstructPath :: Guts p -> Int -> [Int]
+reconstructPath Guts{cameFrom=cf} end = go [] end where
+  go xs x = case IM.lookup x cf of
+    Nothing -> x:xs
+    Just x' -> go (x:xs) x'
 
 -- Priority insert
 pinsert :: Ord a => a -> [a] -> [a]
