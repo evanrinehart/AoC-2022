46f3fe641e9d6dbe9cf211d9803bf30d9d2de5dc
Evan Rinehart <evanrinehart@gmail.com>
2022-12-12 21:07:28 -0600

    fix bug

--- 12/AStar.hs
+++ 12/AStar.hs
@@ -46,13 +46,13 @@ go2 space dest from []     guts = go space guts dest
 go2 space dest from (p:ps) guts = 
   let PathSpace{linkWeight=lw,heuristic=h} = space
       Guts{fScores=fs,gScores=gs,openSet=os,cameFrom=cf} = guts
-      tentativeG = getG guts from + lw from p
-      fscore = tentativeG + h p
+      tentativeG = getG guts from + Fin (lw from p)
+      fscore = finite tentativeG + h p
   in case tentativeG < getG guts p of
     False -> go2 space dest from ps guts
     True  -> go2 space dest from ps
       Guts { fScores  = M.insert p fscore fs
-           , gScores  = M.insert p tentativeG gs
+           , gScores  = M.insert p (finite tentativeG) gs
            , openSet  = pinsert (fscore,p) os
            , cameFrom = M.insert p from cf }
 
@@ -61,8 +61,8 @@ takeBest :: Guts p -> (p, [(Int,p)])
 takeBest Guts{openSet=(_,p):q} = (p,q)
 
 -- G score of point
-getG :: Ord p => Guts p -> p -> Int
-getG guts x = fromMaybe maxBound (M.lookup x (gScores guts))
+getG :: Ord p => Guts p -> p -> Inf Int
+getG guts x = maybe Inf Fin (M.lookup x (gScores guts))
 
 reconstructPath :: Ord p => Guts p -> p -> [p]
 reconstructPath Guts{cameFrom=cf} end = go [] end where
@@ -74,3 +74,29 @@ reconstructPath Guts{cameFrom=cf} end = go [] end where
 pinsert :: Ord a => a -> [a] -> [a]
 pinsert x []     = [x]
 pinsert x (y:ys) = if x <= y then x:y:ys else y : insert x ys
+
+
+
+
+data Inf a = Fin a | Inf deriving (Eq,Show,Read)
+finite Inf     = error "infinite value"
+finite (Fin n) = n
+instance Ord a => Ord (Inf a) where
+  compare Inf     (Fin _) = GT
+  compare (Fin m) (Fin n) = compare m n
+  compare (Fin _) Inf     = LT
+  compare Inf     Inf     = EQ
+instance Num a => Num (Inf a) where
+  fromInteger = Fin . fromIntegral
+  Fin m + Fin n = Fin (m + n)
+  Inf   + _     = Inf
+  _     + Inf   = Inf
+  Fin m * Fin n = Fin (m + n)
+  Inf   * _     = Inf
+  _     * Inf   = Inf
+  negate (Fin n) = Fin (negate n)
+  negate Inf     = error "negative infinity"
+  abs (Fin n) = Fin (abs n)
+  abs Inf     = Inf
+  signum (Fin n) = Fin (signum n)
+  signum Inf     = 1
