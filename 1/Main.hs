9bd21d0440f57d04cda55de177841dbc59460472
Evan Rinehart <evanrinehart@gmail.com>
2022-12-14 14:27:14 -0600

    swap out helper in day 1

--- 1/Main.hs
+++ 1/Main.hs
@@ -14,10 +14,18 @@ sortElves = reverse . sort . map sum
 
 loadData :: FilePath -> IO [[Int]]
 loadData = fmap f . readFile where
-  f = map (map read) . splitOn (=="") . lines 
+  f = map (map read) . splitOn "" . lines
 
+splitOn :: Eq a => a -> [a] -> [[a]]
+splitOn _ [] = []
+splitOn z l@(x:xs)
+  | x==z      = splitOn z xs
+  | otherwise = let (h,t) = break (==z) l in h:(splitOn z t)
+
+{-
 splitOn :: (a -> Bool) -> [a] -> [[a]]
 splitOn _ [] = []
 splitOn f l@(x:xs)
   | f x = splitOn f xs
   | otherwise = let (h,t) = break f l in h:(splitOn f t)
+-}
